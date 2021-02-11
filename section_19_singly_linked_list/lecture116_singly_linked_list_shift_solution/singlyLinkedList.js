badbeb095290d4e45697823084f3ea277398da1e
Ankur Gupta <ankurnecessary@gmail.com>
2021-02-11 19:42:41 +0530

    Lecture 116: Singly linked list: shift solution

---
+++ section_19_singly_linked_list/lecture116_singly_linked_list_shift_solution/singlyLinkedList.js
@@ -0,0 +1,98 @@
+// piece of data - val
+// reference to next node - next
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // To add a node at the end of the singly linked list
+  push(val) {
+
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  // Traversing till the end of the singly linked list
+  /* traverse() {
+    let current = this.head;
+    while (current.next) {
+        current = current.next;
+    }
+
+    return current;
+  }*/
+
+  // Removing last node of the singly linked list
+  pop() {
+
+    if (!this.head) {
+      return undefined;
+    }
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+
+    // Decreasing the length of list
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+
+  }
+
+  // To remove the first item of the list
+  shift() {
+
+    if (!this.head) {
+      return undefined;
+    }
+
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return currentHead;
+  }
+
+}
+
+let list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
