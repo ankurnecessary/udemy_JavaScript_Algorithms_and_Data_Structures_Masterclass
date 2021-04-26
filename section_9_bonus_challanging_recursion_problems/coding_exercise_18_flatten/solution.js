aa8d70fea13a5361416ba7d52ff6b9ccd925ce8c
Ankur Gupta <ankurnecessary@gmail.com>
2021-04-26 19:47:17 +0530

    Coding exercise 18: flatten

---
+++ section_9_bonus_challanging_recursion_problems/coding_exercise_18_flatten/solution.js
@@ -0,0 +1,28 @@
+/*
+
+Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
+
+flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+flatten([[1],[2],[3]]) // [1,2,3]
+flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
+
+*/
+
+// Way 1
+function flatten(arr) {
+  return arr.flat(Infinity);
+}
+
+// Way 2 - IN PROGRESS
+function flatten(oldArr) {
+  var newArr = []
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]))
+    } else {
+      newArr.push(oldArr[i])
+    }
+  }
+  return newArr;
+}
