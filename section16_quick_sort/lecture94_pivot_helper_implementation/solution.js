5dfd7d5902f248ce3b2f9f1b81c78d97d256e8d8
Ankur Gupta <ankurnecessary@gmail.com>
2021-05-30 20:16:24 +0530

    Lecture 94: Pivot helper implementation

--- section16_quick_sort/lecture94_pivot_helper_implementation/solution.js
+++ section16_quick_sort/lecture94_pivot_helper_implementation/solution.js
@@ -1,16 +1,14 @@
 function pivot(arr, start, end) {
-  if (!arr.length)
+  if (!arr.length) {
     return;
+  }
   const pivot = arr[start];
-  let swapIdx = start + 1;
+  let swapIdx = start;
 
   for (let i = start + 1; i <= end; i++) {
     if (arr[i] < pivot) {
+      swapIdx++;
       [arr[swapIdx], arr[i]] = [arr[i], arr[swapIdx]];
-      swapIdx += 1;
-    }
-    if (i === end) {
-      swapIdx -= 1;
     }
   }
 
@@ -19,7 +17,7 @@ function pivot(arr, start, end) {
   return swapIdx;
 }
 
-// var arr = [10, 3, 23, 5, 14, 6, 100, 45, 69, 58];
+// var arr = [10, 3, 23, 5, 14, 6, 100, 45, 69, 58]; // 3
 // var arr = [9, 4, 8, 2, 1, 5, 7, 6, 3]; // 8
 var arr = [4, 8, 2, 1, 5, 7, 6, 3]; // 3
 pivot(arr, 0, arr.length - 1);
